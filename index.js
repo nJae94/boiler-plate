047e1cf78a437577ce363b20a7a63605113e1ff9
sunjae <azuretic94@gmail.com>
2020-05-27 00:03:59 +0900

    로그인 api 테스트

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express =require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const {User}= require("./models/User");
+
+//application/x--www-form-urlencoded 분석
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
 
 //mongodb+srv://sunjae:<password>@boilerplate-thzoj.mongodb.net/test?retryWrites=true&w=majority
 
@@ -12,6 +20,18 @@ mongoose.connect('mongodb+srv://sunjae:wldmsl0516@boilerplate-thzoj.mongodb.net/
     useFindAndModify:false,
 }).then(()=> console.log("연결 성공")).catch(err => console.log(err));
 
-app.get('/',(req,res) => res.send('hello'));
+app.get('/',(req,res) => res.send('hello its me'));
 
-app.listen(port,()=>console.log('성공'));
+app.post('/register',(req,res)=> {
+
+    const user = new User(req.body);
+
+    user.save((err,doc)=> {
+        if(err) return res.json({success: false, err});
+
+        return res.status(200).json({success:true});
+    })
+
+});
+
+app.listen(port,()=>console.log('성공')); 
